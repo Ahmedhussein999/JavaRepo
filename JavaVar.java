740ff3e08bac7b05929af54beec18ef2189120ab
Ahmed hussein <Ahmed.Hussein5@student.tdsb.on.ca>
2024-04-11 17:10:15 -0400

    New file added
    New java file added

---
+++ JavaVar.java
@@ -0,0 +1,34 @@
+public class JavaVar{
+//datatype
+//Class level vars 
+
+    public static void main(String[] args) throws Exception {
+    
+        int games = 3;
+        double allowence = 40.50;
+        long battlepass;//we define battle pass
+
+        battlepass = 15;//we assigned a value to battle pass
+
+
+        //prints out the out var values  along with a string that explains our values
+        System.out.println("Number of games I own =" + games);
+        System.out.println("my mounthy allowance =" + allowence);
+        System.out.println("cost of a sinlge =" + battlepass);
+         
+    }
+}
+
+//QUESTION #2
+
+//With our current Allowance, how many battle passes will we be able to buy?
+//Comment what you did, and why/how
+//QUESTION #1
+
+//the answer will be 2.7 becouse it would be 40.50/15 = 2.7
+
+//What is the total cost of battle passes
+//Comment what you did, and why/how
+//Add your code to github by committing and pushing!
+
+//the total cost of the battle pass will be 45 because 15x3 = 45
